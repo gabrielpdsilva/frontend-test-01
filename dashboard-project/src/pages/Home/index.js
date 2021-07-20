dbb7066fb416714116143de0e06b42eb15e65568
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2021-07-20 16:58:54 -0300

    Defining logic about creating new graphic; creating modal component (not finished yet)

--- dashboard-project/src/pages/Home/index.js
+++ dashboard-project/src/pages/Home/index.js
@@ -1,24 +1,47 @@
-import { Fab, Input } from '@material-ui/core';
-import plusIcon from '../../design/assets/plusIcon.png';
-import delfos_Intelligent_maintenance from '../../design/assets/delfos_Intelligent_maintenance.png';
-import React from 'react';
+import React, { useState } from 'react';
 import Graphic from '../../components/Graphic';
 
+import {RootDiv} from './styles';
+import SearchAppBar from '../../components/SearchAppBar';
+import TransitionsModal from '../../components/TransitionsModal';
+
 const Home = () => {
+    const [graphicTitle, setGraphicTitle] = useState('');
+    const [graphicValues, setGraphicValues] = useState([]);
+    // TODO remove hardcoded values
+    const [graphics, setGraphics] = useState([
+        {title: 'A', values: [3, 2, 5]},
+        {title: 'B', values: [10, 5, 6]},
+    ]);
+
+    function addGraphic(title, values) {
+        const newGraphic = {
+            title: title,
+            values: values
+        };
+        setGraphics([...graphics, newGraphic]);
+    }
     // TODO this structure is just for test and will be refactored soon
     return (
-        <div style={{backgroundColor: '#36394c', height: '100vh'}}>
-            <div style={{width: 100}}>
-                <img src={delfos_Intelligent_maintenance} width={200} height={150}/>
-                <Input placeholder={"Search..."}/>
-            </div>
-            <Graphic/>
-            <Fab color="primary" aria-label="add" onClick={() => {
-                // TODO implement
-            }}>
-                <img src={plusIcon} width={30} height={30}/>
-            </Fab>
-        </div>
+        <RootDiv>
+            <SearchAppBar/>
+            {
+                graphics.map((graphic, index) => {
+                    return (
+                        <Graphic key={index} title={graphic.title} data={graphic.values}/>
+                    )
+                })
+            }
+            <button onClick={() => addGraphic(graphicTitle, graphicValues)}>
+                <p>Salvar</p>
+            </button>
+            <TransitionsModal
+                title={"Novo gráfico"}
+                description={"Digite os dados do novo gráfico."}
+                setGraphicTitle={setGraphicTitle}
+                setGraphicValues={setGraphicValues}
+            />
+        </RootDiv>
     )
 }
 
