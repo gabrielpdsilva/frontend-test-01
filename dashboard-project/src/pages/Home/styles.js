842b37a93a06dc0ee9fd49eed27f90b9e6dfb91e
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2021-07-20 18:16:05 -0300

    added style for text that says that there is no graphic

--- dashboard-project/src/pages/Home/styles.js
+++ dashboard-project/src/pages/Home/styles.js
@@ -2,5 +2,14 @@ import styled from 'styled-components';
 import COLORS from '../../design/colors';
 
 export const RootDiv = styled.div`
+    height: 100vh;
     background-color: ${COLORS.dark_gray};
+`;
+
+export const NoGraphicsText = styled.div`
+    margin: 30px;
+    font-size: 30px;
+    font-weight: bold;
+    color: ${COLORS.white};
+    text-align: center;
 `;
