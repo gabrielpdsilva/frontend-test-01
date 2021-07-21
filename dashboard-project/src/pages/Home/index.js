14551cbb0f0389626f18dac1ad5cacb0d64fbbaa
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2021-07-21 17:50:08 -0300

    started refactoring style

--- dashboard-project/src/pages/Home/index.js
+++ dashboard-project/src/pages/Home/index.js
@@ -1,12 +1,46 @@
 import React, { useState } from 'react';
 import Graphic from '../../components/Graphic';
-import {RootDiv, NoGraphicsText, WelcomeText, InformationText} from './styles';
 import SearchAppBar from '../../components/SearchAppBar';
 import TextField from '@material-ui/core/TextField';
 import { Button, Fab } from '@material-ui/core';
 import plusIcon from '../../design/assets/plusIcon.png';
+import { makeStyles } from '@material-ui/core/styles';
+import COLORS from '../../design/colors';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        overflow: 'auto',
+        height: '100vh',
+        maxHeight: '100vh',
+        backgroundColor: COLORS.dark_gray,
+    },
+    fabContainer: {
+        alignSelf: 'flex-end',
+        position: 'absolute',
+        bottom: 30,
+        right: 30
+    },
+    welcomeText: {
+        fontSize: 50,
+        color: COLORS.soft_yellow,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    informationText: {
+        fontSize: 20,
+        color: COLORS.white,
+        textAlign: 'center',
+    },
+    noGraphicsText: {
+        fontSize: 20,
+        color: COLORS.white,
+        textAlign: 'center',
+    },
+  }));
 
 const Home = () => {
+    const classes = useStyles();
+
     const [graphicTitle, setGraphicTitle] = useState('');
     const [graphicValues, setGraphicValues] = useState([]);
     const [isAddFormVisible, setAddFormVisible] = useState(false);
@@ -34,9 +68,11 @@ const Home = () => {
 
     function renderFabIcon() {
         return (
-            <Fab color="primary" aria-label="add" onClick={() => setAddFormVisible(true)}>
-                <img src={plusIcon} width={30} height={30}/>
-            </Fab>
+            <div className={classes.fabContainer}>
+                <Fab color="primary" aria-label="add" onClick={() => setAddFormVisible(true)}>
+                    <img src={plusIcon} width={30} height={30}/>
+                </Fab>
+            </div>
         )
     }
 
@@ -89,15 +125,15 @@ const Home = () => {
     }
 
     return (
-        <RootDiv>
+        <div className={classes.root}>
             <SearchAppBar/>
             <div>
-                <WelcomeText>Bem-vindo ao Dashboard!</WelcomeText>
-                <InformationText>Você já pode adicionar, editar e remover quantos gráficos desejar.</InformationText>
+                <h1 className={classes.welcomeText}>Bem-vindo ao Dashboard!</h1>
+                <h2 className={classes.informationText}>Você já pode adicionar, editar e remover quantos gráficos desejar.</h2>
             </div>
             {isAddFormVisible && renderAddForm()}
             {isEditFormVisible && renderEditForm()}
-            { graphics.length == 0 && <NoGraphicsText>Nenhum gráfico foi adicionado.</NoGraphicsText> }
+            { graphics.length == 0 && <p className={classes.noGraphicsText}>Nenhum gráfico foi adicionado.</p> }
             {
                 graphics.map((graphic, index) => {
                     return (
@@ -115,7 +151,7 @@ const Home = () => {
                 })
             }
             {!isAddFormVisible && renderFabIcon()}
-        </RootDiv>
+        </div>
     )
 }
 
